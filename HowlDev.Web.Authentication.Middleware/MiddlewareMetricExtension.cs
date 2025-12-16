f00997e799ecb730912eb9d13f48eacc38dadea9
Cody-Howell <codyhowell44@gmail.com>
2025-12-16 13:44:59 -0700

    Removed middleware extension

--- HowlDev.Web.Authentication.Middleware/MiddlewareMetricExtension.cs
+++
@@ -1,7 +0,0 @@
-public static class AuthLibraryExtensions
-{
-    public static MeterProviderBuilder AddAuthMiddlewareMetrics(this MeterProviderBuilder builder)
-    {
-        return builder.AddMeter("HowlDev.Web.Authentication.Middleware");
-    }
-}
